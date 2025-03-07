7099b3390460a881f16759f9fbf7e2f5383a6460
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-03-07 23:04:32 +0100

    define based layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { Toast, initNotifierContext } from '$lib/comp/notifier';
 	import '../app.css';
 	let { children } = $props();
+
+	const notifications = initNotifierContext();
 </script>
 
 <ParaglideJS {i18n}>
 	{@render children()}
+
+	<div class="fixed right-4 bottom-4 z-50">
+		{#each $notifications as toast (toast.id)}
+			<Toast>
+				{toast.message}
+			</Toast>
+		{/each}
+	</div>
 </ParaglideJS>
