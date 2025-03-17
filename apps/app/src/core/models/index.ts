78f319f3587b75a7cf16d232346d1d2acfa394db
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-03-17 16:32:49 +0100

    export schemas to use in other libs

---
+++ apps/app/src/core/models/index.ts
@@ -0,0 +1,7 @@
+export * from './user';
+export * from './form';
+export * from './apiKey';
+export * from './otp';
+export * from './session';
+export * from './user';
+export * from './team';
--- apps/app/tsup.config.ts
+++ apps/app/tsup.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/exports/client.ts"],
-  format: ["cjs", "esm"],
-  external: ["valibot", "bson"],
-  dts: true,
-  clean: true,
-  splitting: false,
+	entry: ['./src/exports/lib.ts'],
+	outDir: 'dist/lib',
+	format: ['cjs', 'esm'],
+	external: ['valibot', 'bson'],
+	dts: true,
+	clean: true,
+	splitting: false
 });
